4fc8af744d4b6c1773adefed5d794dd3272f72e8
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-06-04 18:54:33 -0400

    Update main.tf

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,7 +1,17 @@
 provider "aws" {
   region = "us-east-1"
 }
- 
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-nicolas25"
+    key            = "vpcactions/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
